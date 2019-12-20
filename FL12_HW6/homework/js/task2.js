e628b50bfec2092c2aaad8dc694d9f8fa86788f2
Gusvarov <omelchenko.nazarii@gmail.com>
2019-12-20 20:23:53 +0200

    Finished Task 6

---
+++ FL12_HW6/homework/js/task2.js
@@ -0,0 +1,24 @@
+let a = parseInt(prompt('a?'));
+let b = parseInt(prompt('b?'));
+let c = parseInt(prompt('c?'));
+
+if ( !Number(a) && a !== 0 || !Number(b) && b !== 0 || !Number(c) && c !== 0 ) {
+    alert('input values should be ONLY numbers');
+} else {
+    if (a <= 0 || b <= 0 || c <= 0) {
+        alert('A triangle must have 3 sides with a positive definite length');
+    } 
+    if ( a + b > c && a + c > b && c + b > a ) {
+        if (a === b && b === c && c === a) {
+            console.log('Equilateral triangle');
+        } else if (a === b || b === c || c === a) {
+            console.log('Isosceles triangle');
+        } else if (a !== b || b !== c || c !== a) {
+            console.log('Scalene triangle');
+        } 
+    } else {
+        alert('Triangle doesn’t exist');
+    }
+}
+
+
